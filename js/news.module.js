429cd79437c8834074e8d0bafb754872175818b0
Thang Tran <thangtd1993@gmail.com>
2018-05-16 16:33:06 +0700

    fix news css

--- js/news.module.js
+++ js/news.module.js
@@ -43,10 +43,10 @@ function getListNews() {
             $('#list-news').html('');
             $.each(newsData, function (k, v) {
                 var html = '<div class="col-12 col-lg-6 col-md-6 news-item" data-id="'+v.news_id+'">';
-                html += '<div class="col-5 img-news" data-id="'+v.news_id+'">';
+                html += '<div class="col-12 col-lg-5 col-md-5 col-sm-6 img-news" data-id="'+v.news_id+'">';
                 html += '<img src="' + v.news_image + '" alt="'+ v.news_name +'">';
                 html += '</div>';
-                html += '<div class="col-7 news-content">';
+                html += '<div class="col-12 col-lg-7 col-md-7 col-sm-5 news-content">';
                 html += '<h6 class="news-title">'+ v.news_name +'</h6>';
                 html += '<p class="news-note">'+ v.news_note +'</p>';
                 html += '<div class="news-description">'+ v.news_description +'</div>';
