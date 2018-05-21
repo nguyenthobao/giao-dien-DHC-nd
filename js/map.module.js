43e631dd706dd3b50d65a42e84a8c4c2f424b36b
Thang Tran <thangtd1993@gmail.com>
2018-05-21 14:40:17 +0700

    redirect app map

--- js/map.module.js
+++ js/map.module.js
@@ -1,5 +1,40 @@
 $(document).ready(function () {
+    var isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function() {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+        }
+    };
+
+    urlAndroid = 'https://itunes.apple.com/us/app/dhc-travel/id1381272202?l=vi&ls=1&mt=8';
+    urlIOs = 'https://itunes.apple.com/us/app/dhc-travel/id1381272202?l=vi&ls=1&mt=8';
+
+
     $('#tab2').change(function () {
-        wheelzoom(document.querySelector('img.map', {initialZoom: 2}));
+        console.log(isMobile.any()[0]);
+        /*wheelzoom(document.querySelector('img.map', {initialZoom: 2}));*/
+        if(isMobile.any()[0] == 'Android' && urlAndroid != '')
+        {
+            window.location.href = urlAndroid;
+        }
+
+        if((isMobile.any()[0] == 'iPhone' || isMobile.any()[0] == 'iPad' || isMobile.any()[0] == 'iPod') && urlIOs != '')
+        {
+            window.location.href = urlIOs;
+        }
     });
 });
