1330a50235529d9d852c9960ea7032dac8b0da2c
bao <thobaocss@gmail.com>
2018-07-20 16:22:14 +0700

    update dhc user

--- js/map.module.js
+++ js/map.module.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
     var scaleY = 1;
     var x = -1, y = -1;
     getOriginal1(15.971174, 108.017871, 15.968976, 108.018555, 3725, 2183 + 15, 4311, 4103 + 15);
-    $('body').on('mouseup', '#mapdhc', function () {
+    $('body').on('mousedown', '#mapdhc', function () {
         var p = document.getElementById('marker');
         var style = p.currentStyle || window.getComputedStyle(p);
         var u = (style.marginTop).substring(0, 4);
@@ -16,7 +16,7 @@ $(document).ready(function () {
         if (Math.abs(u - y)>30 || Math.abs(v - x) >30)
             $('#marker').hide();
     });
-    $('body').on('mousedown', '#mapdhc', function () {
+    $('body').on('mouseup', '#mapdhc', function () {
         $('#marker').show();
     });
     $('#tab2').change(function () {
