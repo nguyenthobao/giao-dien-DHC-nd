3713ea8a703ef4206b5412a9dd672c5e74466f5a
bao <thobaocss@gmail.com>
2018-07-20 14:58:35 +0700

    update dhc user

--- js/map.module.js
+++ js/map.module.js
@@ -13,8 +13,8 @@ $(document).ready(function () {
             //     "lat = " + position.coords.latitude + "lng = " + position.coords.longitude);
             // var x= parseFloat( getXPixcelValue(position.coords.latitude,position.coords.longitude));
             // var y= parseFloat( getYPixcelValue(position.coords.latitude,position.coords.longitude));
-            var x= parseFloat( getXPixcelValue(15.971174, 108.017871));
-            var y= parseFloat( getYPixcelValue(15.971174, 108.017871));
+            var x= parseFloat( getXPixcelValue(15.971174, 108.017871)/(9798/$('#mapdhc')[0].width));
+            var y= parseFloat( getYPixcelValue(15.971174, 108.017871)/(7046/ $('#mapdhc')[0].height));
             if (x > $('#mapdhc')[0].width || x < 0 || y > $('#mapdhc')[0].height || y < 0) {
                 $('#marker').hide();
             } else{
@@ -22,7 +22,7 @@ $(document).ready(function () {
                 $('#marker').css("margin-left", x+"px");
                 $('#marker').show();
             }
-            alert($('#mapdhc')[0].width+ '......'+ $('#mapdhc')[0].height);
+            alert(x+ '......'+ y);
         };
 
         var tryAPIGeolocation = function() {
