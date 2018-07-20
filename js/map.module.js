97d7a27c29508eacf53db0b6f6e9e16a756257a0
bao <thobaocss@gmail.com>
2018-07-20 15:24:31 +0700

    update dhc user

--- js/map.module.js
+++ js/map.module.js
@@ -13,8 +13,8 @@ $(document).ready(function () {
             //     "lat = " + position.coords.latitude + "lng = " + position.coords.longitude);
             // var x= parseFloat( getXPixcelValue(position.coords.latitude,position.coords.longitude));
             // var y= parseFloat( getYPixcelValue(position.coords.latitude,position.coords.longitude));
-            var x= parseFloat( getXPixcelValue(15.971174, 108.017871)/(9798/2048));
-            var y= parseFloat( getYPixcelValue(15.971174, 108.017871)/(7046/ 1473));
+            var x= parseFloat( getXPixcelValue(15.968967, 108.018552)/(9798/2048));
+            var y= parseFloat( getYPixcelValue(15.968967, 108.018552)/(7046/ 1473));
             if (x > $('#mapdhc')[0].width || x < 0 || y > $('#mapdhc')[0].height || y < 0) {
                 $('#marker').hide();
             } else{
