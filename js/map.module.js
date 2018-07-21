ddaf68997fdfff86e666b37a75c368b6ad673d63
bao <thobaocss@gmail.com>
2018-07-21 16:00:34 +0700

    update dhc user

--- js/map.module.js
+++ js/map.module.js
@@ -45,10 +45,11 @@ $(document).ready(function () {
                     document.getElementById('marker').scrollIntoView();
                 }
             }else{
-                alert($('#mapdhc')[0].getBoundingClientRect().width+'..'+$('#mapdhc')[0].getBoundingClientRect().height);
+                // alert($('#mapdhc')[0].getBoundingClientRect().width+'..'+$('#mapdhc')[0].getBoundingClientRect().height);
                 x = parseFloat(getXPixcelValue(15.967649, 108.019897) / (9798 / $('#mapdhc')[0].getBoundingClientRect().width));
                 y = parseFloat(getYPixcelValue(15.967649, 108.019897) / (7046 / $('#mapdhc')[0].getBoundingClientRect().height));
-               if (x >$('#mapdhc')[0].getBoundingClientRect().width || x < 0 || y > $('#mapdhc')[0].getBoundingClientRect().height || y < 0) {
+               alert(x+'..'+y);
+                if (x >$('#mapdhc')[0].getBoundingClientRect().width || x < 0 || y > $('#mapdhc')[0].getBoundingClientRect().height || y < 0) {
                    // $('#marker').hide();
                 } else {
                     $('#marker').css("margin-top", (y - 15) + "px");
