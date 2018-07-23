add316219e5f8ba3cd4b4ada5b81f52042a10f89
bao <thobaocss@gmail.com>
2018-07-23 14:29:08 +0700

    update dhc user

--- js/map.module.js
+++ js/map.module.js
@@ -53,7 +53,7 @@ $(document).ready(function () {
                 if (x >($('#mapdhc').css('width')).substring(0,4) || x < 0 || y >($('#mapdhc').css('height')).substring(0,4) || y < 0) {
                    // $('#marker').hide();
                 } else {
-                    $('#marker').css("margin-top", (y-53) + "px");
+                    $('#marker').css("margin-top", (y-200) + "px");
                     $('#marker').css("margin-left", x+ "px");
                     $('#marker').show();
                     document.getElementById('marker').scrollIntoView();
