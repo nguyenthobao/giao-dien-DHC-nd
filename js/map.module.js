97217d0196fce7585f919df78ec3da157d60b33f
bao <thobaocss@gmail.com>
2018-07-26 11:40:47 +0700

    update dhc user

--- js/map.module.js
+++ js/map.module.js
@@ -111,8 +111,8 @@ $(document).ready(function () {
     });
     $('body').on('change','#search_place',function(){
         console.log($('#search_place option:selected'));
-        x = parseFloat(getXPixcelValue($('#search_place option:selected').data('left'),$('#search_place option:selected').data('top')) / (9798 / ($('#mapdhc').css('width')).substring(0,4)));
-        y = parseFloat(getYPixcelValue($('#search_place option:selected').data('left'),$('#search_place option:selected').data('top')) / (7046 / ($('#mapdhc').css('height')).substring(0,4)));
+        x = parseFloat(getXPixcelValue($('#search_place option:selected').data('left'),$('#search_place option:selected').data('top')) / (9798 / 2048));
+        y = parseFloat(getYPixcelValue($('#search_place option:selected').data('left'),$('#search_place option:selected').data('top')) / (7046 /1473));
         $('#marker').show();
         $('#marker').css("margin-top", (y - 15) + "px");
         $('#marker').css("margin-left", x + "px");
