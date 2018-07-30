463df5939ecd0ae6ebaec56cda79b461f70bad01
bao <thobaocss@gmail.com>
2018-07-30 11:23:15 +0700

    update dhc user

--- js/map.module.js
+++ js/map.module.js
@@ -140,7 +140,7 @@ $(document).ready(function () {
         }
 
         $('#marker').css("margin-top", (y) + "px").css("margin-left", (x) + "px");
-        setTimeout(($('#marker')).scrollTo(x-400,y-600),100);
+        setTimeout((document.getElementById('mapdhc')).scrollTo(x,y),100);
         $('#marker').show();
     });
     $('body').on('click', '#download', function () {
