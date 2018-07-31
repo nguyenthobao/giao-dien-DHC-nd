07139bd808b900495143ebdd035805c9f2bac116
bao <thobaocss@gmail.com>
2018-07-31 15:52:56 +0700

    update dhc user

--- js/map.module.js
+++ js/map.module.js
@@ -206,7 +206,7 @@ $(document).ready(function () {
                 html += '</div>';
                 html += '</div>';
                 $('#form-body').html(html);
-                window.scrollTo(0,0);
+                (document.getElementById('modalForm')).scrollIntoView();
             },
             error: function (e) {
                 alert('Có lỗi');
