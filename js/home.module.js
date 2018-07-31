9818388fef0e7929548abf644c1fc58f5efc5232
bao <thobaocss@gmail.com>
2018-07-31 12:15:17 +0700

    update dhc user

--- js/home.module.js
+++ js/home.module.js
@@ -189,7 +189,10 @@ $(document).ready(function () {
             }
         });
     });
-    $('body').on('mousedown,mouseup','#content2', function () {
+    $('body').on('mousedown','#content2', function () {
+        $('html').attr('style','');
+    });
+    $('body').on('mouseup','#content2', function () {
         $('html').attr('style','');
     });
     $('body').on('click', '.fixed-top', function () {
