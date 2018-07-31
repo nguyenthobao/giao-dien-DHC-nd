db201d189cbb559a25cfea87ae2047db77c0a9e5
bao <thobaocss@gmail.com>
2018-07-31 09:14:20 +0700

    update dhc user

--- js/map.module.js
+++ js/map.module.js
@@ -142,7 +142,7 @@ $(document).ready(function () {
         $('#marker').css("margin-top",y + "px").css("margin-left",x + "px");
          $('#marker').show();
         // (document.getElementById('marker')).scrollIntoView();
-        setTimeout($('#content2 .container').animate({
+        setTimeout($('#content2').animate({
             scrollTop:y,
             scrollLeft:x
         }),100);
