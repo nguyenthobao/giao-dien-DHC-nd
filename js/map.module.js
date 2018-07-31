9f48fd35726d0a9760af1970b60e5501a45072bd
bao <thobaocss@gmail.com>
2018-07-31 17:27:21 +0700

    update dhc user

--- js/map.module.js
+++ js/map.module.js
@@ -43,7 +43,6 @@ $(document).ready(function () {
                     $('#marker').css("margin-top", (y - 15) + "px");
                     $('#marker').css("margin-left", x + "px");
                     $('#marker').show();
-                    document.getElementById('marker').scrollIntoView();
                 }
                 var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
                 if (width>1000) $('#download').hide();
