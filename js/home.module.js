49fa45aea17826c1734a2bc2a73a7de6077554f4
bao <thobaocss@gmail.com>
2018-08-02 14:54:49 +0700

    update dhc user

--- js/home.module.js
+++ js/home.module.js
@@ -86,7 +86,7 @@ $(document).ready(function () {
                 html_select += '<option data-top="' + v.long + '" data-left="' + v.lat + '" >' + v.point_name + '</option>';
                 html_marker += '<div class="div_marker" data-id="' + v.point_id + '" data-lat="' + v.lat + '" data-long="' + v.long + '" style="margin-top:' + y + 'px; margin-left: ' + (x-75) + 'px;    position: absolute; ">' +
                     '<img src="' + url + '" data-x="' + x + '" data-y="'+y+'" style="max-width: 20000px; width: 18px;margin-left: 75px; height: 25px" class="point_important img-fluid map" alt="">' +
-                    '<label data-id="' + v.point_id + '" id="label_' + x + '" class="label_instant">' + v.point_name + '</label><br>' +
+                    '<br><label data-id="' + v.point_id + '" id="label_' + x + '" class="label_instant">' + v.point_name + '</label><br>' +
                     '<img data-id="' + v.point_id + '" id="img_' + x + '" src="' + pointImage[0] + '" class="img_instant img-fluid map" alt="">' +
                     '</div>';
             });
