2dd3e6778d742c64258d39a0c23ce214cb8003b9
bao <thobaocss@gmail.com>
2018-08-02 16:36:35 +0700

    update dhc user

--- js/map.module.js
+++ js/map.module.js
@@ -213,10 +213,10 @@ $(document).ready(function () {
                 point_id: pointId,
             }),
             success: function (result) {
-                if ((document.getElementById("content2")).offsetLeft > 0 && x > 0 && y > 0)
-                    $('#modalForm').attr('style', 'margin-top:' + (y - 50) + 'px;margin-left:' + (x - 300) + 'px');
-                else
-                    $('#modalForm').attr('style', '');
+                // if ((document.getElementById("content2")).offsetLeft > 0 && x > 0 && y > 0)
+                //     $('#modalForm').attr('style', 'margin-top:' + (y - 50) + 'px;margin-left:' + (x - 300) + 'px');
+                // else
+                //     $('#modalForm').attr('style', '');
                 $('#modalForm').modal('show');
                 $('#modalFormLabel').text(result.data.result.point_name);
                 var pointImage = JSON.parse(result.data.result.point_images);
