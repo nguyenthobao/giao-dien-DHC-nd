fd3687d5a30fbf4ab2fa0c0691abe588c2521698
bao <thobaocss@gmail.com>
2018-08-03 15:05:49 +0700

    update dhc user

--- js/map.module.js
+++ js/map.module.js
@@ -41,28 +41,28 @@ $(document).ready(function () {
         scrollTop: 890,
         scrollLeft: 950
     }), 100);
-    // if (ua.indexOf('safari') > -1) {
-    //     sTimeout = setTimeout(function () {
-    //         $('body').animate({
-    //             scrollTop: y - 150,
-    //             scrollLeft: x - 200
-    //         })
-    //     }.bind(this), 1000);
-    //     Function.prototype.bind = function (parent) {
-    //         var f = this;
-    //         var args = [];
-    //
-    //         for (var a = 1; a < arguments.length; a++) {
-    //             args[args.length] = arguments[a];
-    //         }
-    //
-    //         var temp = function () {
-    //             return f.apply(parent, args);
-    //         };
-    //
-    //         return (temp);
-    //     }
-    // }
+    if (ua.indexOf('safari') > -1) {
+        sTimeout = setTimeout(function () {
+            $('body').animate({
+                scrollTop: y - 150,
+                scrollLeft: x - 200
+            })
+        }.bind(this), 1000);
+        Function.prototype.bind = function (parent) {
+            var f = this;
+            var args = [];
+
+            for (var a = 1; a < arguments.length; a++) {
+                args[args.length] = arguments[a];
+            }
+
+            var temp = function () {
+                return f.apply(parent, args);
+            };
+
+            return (temp);
+        }
+    }
     $('#download').show();
     $('#div_search').show();
     getOriginal1(15.971174, 108.017871, 15.968976, 108.018555, 3725, 2183 + 15, 4311, 4103 + 15);
@@ -288,7 +288,7 @@ $(document).ready(function () {
     });
     $('body').on('click', '#mapdhc', function (e) {
         var offset = $(this).offset();
-        if (e.pageX - offset.left > 1000 || e.pageY - offset.top > 1000)
+        if (e.pageX - offset.left > 1000 || e.pageY - offset.top > 1200)
             $('html').attr('style', '');
     });
     $('body').on('click', '#download', function () {
