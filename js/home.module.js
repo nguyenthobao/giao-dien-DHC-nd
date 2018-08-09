28edd4ac6f676eddcd1a318055d382a50f0c65d1
bao <thobaocss@gmail.com>
2018-08-09 10:05:04 +0700

    update dhc user

--- js/home.module.js
+++ js/home.module.js
@@ -54,6 +54,7 @@ $(document).ready(function () {
                 html = '<div class="item-point" data-id="' + v.point_id + '">';
                 var pointImage = JSON.parse(v.point_images);
                 console.log(v.point_images,pointImage,pointImage[0]);
+                if(pointImage[0]!='[]')
                 pointImage[0]=(pointImage[0]).splice(0,4)+'s'+ (pointImage[0]).splice(4);
                 html += '<img src="' + pointImage[0] + '" class="img-thumbnail" alt="' + v.point_name + '">';
                 html += '<h5 class="point-title">' + v.point_name + '</h5>';
@@ -86,6 +87,7 @@ $(document).ready(function () {
             var html_select = '<option>Chọn địa điểm</option>', html_marker = '';
             $.each(pointData, function (k, v) {
                 var pointImage = JSON.parse(v.point_images);
+                if(pointImage[0]!='[]')
                 pointImage[0]=(pointImage[0]).splice(0,4)+'s'+ (pointImage[0]).splice(4);
                 var url = '';
                 x = parseFloat(v.lat / parseFloat(9798 / 2048));
