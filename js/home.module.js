55b57443d1d08d0a3d118c5af9f7f2b30688f434
bao <thobaocss@gmail.com>
2018-08-09 10:23:11 +0700

    update dhc user

--- js/home.module.js
+++ js/home.module.js
@@ -54,8 +54,8 @@ $(document).ready(function () {
                 html = '<div class="item-point" data-id="' + v.point_id + '">';
                 var pointImage = JSON.parse(v.point_images);
                 console.log(pointImage[0], typeof pointImage[0]);
-                // if(pointImage[0]!=undefined)
-                // pointImage[0]=(pointImage[0]).toString().splice(0,4)+'s'+ (pointImage[0]).toString().splice(4);
+                if(pointImage[0]!=undefined)
+                pointImage[0]=(pointImage[0]).slice(0,4)+'s'+ (pointImage[0]).slice(4);
                 html += '<img src="' + pointImage[0] + '" class="img-thumbnail" alt="' + v.point_name + '">';
                 html += '<h5 class="point-title">' + v.point_name + '</h5>';
                 html += '</div>';
@@ -87,8 +87,8 @@ $(document).ready(function () {
             var html_select = '<option>Chọn địa điểm</option>', html_marker = '';
             $.each(pointData, function (k, v) {
                 var pointImage = JSON.parse(v.point_images);
-                // if(pointImage[0]!=undefined)
-                // pointImage[0]=(pointImage[0]).toString().splice(0,4)+'s'+ (pointImage[0]).toString().splice(4);
+                if(pointImage[0]!=undefined)
+                pointImage[0]=(pointImage[0]).slice(0,4)+'s'+ (pointImage[0]).slice(4);
                 var url = '';
                 x = parseFloat(v.lat / parseFloat(9798 / 2048));
                 y = parseFloat(v.long / parseFloat(7046 / heightmap));
