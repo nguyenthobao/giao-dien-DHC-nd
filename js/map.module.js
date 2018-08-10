e1cdb9da928d8206a3b88ca3b26d064c8c2bfa0a
bao <thobaocss@gmail.com>
2018-08-10 14:18:54 +0700

    update dhc user

--- js/map.module.js
+++ js/map.module.js
@@ -869,7 +869,7 @@ function generate_way() {
     var ctx = (document.getElementById('can')).getContext('2d');
     ctx.beginPath();
     ctx.moveTo(0,0);
-    ctx.lineTo(width,height);
+    ctx.lineTo(375,height);
     ctx.stroke();
     top_before=100, left_before=100;
     var start_draw=0;
