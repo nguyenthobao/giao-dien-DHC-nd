3e98f1d00a0e48a21a4cbf4937b8e99e4bbe197b
bao <thobaocss@gmail.com>
2018-08-13 09:56:08 +0700

    update dhc user

--- js/map.module.js
+++ js/map.module.js
@@ -50,6 +50,7 @@ $(document).ready(function () {
     $('#tab2').change(function () {
         into_map();
     });
+
     function into_map() {
         // var tryAPIGeolocation = function () {
         //     jQuery.post("https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyBZKcLL5G9t6MGhYHwl7JN50LEhvDysIZ8", function (success) {
@@ -90,7 +91,6 @@ $(document).ready(function () {
                     scroll(750, 690, 950, 890);
                     $('#marker').hide();
                 }
-
                 var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
                 if (width > 1000) $('#download').hide();
             }
@@ -928,7 +928,7 @@ function generate_way(listP) {
     if (document.getElementById('can'))
         (document.getElementById('can')).remove();
     if (isMobile.any() == null) {
-        var marginTop = (minH - 1540);
+        var marginTop = (minH - 1510);
         var marginLeft = (minW + 10);
     } else {
         if ((window.innerWidth > 0) ? window.innerWidth : screen.width > 380)
