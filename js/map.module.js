84c616e71acd91dcef76de77b3448d01ae35d7dd
bao <thobaocss@gmail.com>
2018-08-28 10:50:59 +0700

    update dhc user

--- js/map.module.js
+++ js/map.module.js
@@ -30,8 +30,8 @@ var that = null,
     urlAndroid = 'https://play.google.com/store/apps/details?id=vn.anvui.hotspringpark';
 urlIOs = 'https://itunes.apple.com/us/app/dhc-travel/id1381272202?l=vi&ls=1&mt=8';
 $(document).ready(function () {
-    var elem = document.getElementById('mapdhc');
-    new Zoom(elem);
+    // var elem = document.getElementById('mapdhc');
+    // new Zoom(elem);
     alert('Click " + " hoặc " - " để phóng to hoặc thu nhỏ bản đồ' );
     $('#tab2').prop('checked', true);
     $('main > label').hide();
