0567482af2eda58437a08da1ce00458120c92b4d
bao <thobaocss@gmail.com>
2018-08-31 10:38:00 +0700

    update dhc user

--- js/home.module.js
+++ js/home.module.js
@@ -48,7 +48,7 @@ $(document).ready(function () {
     document.documentElement.addEventListener('touchmove', function (e) {
         alert(e.touches.length);
         if (e.touches.length > 1)
-            alert(e.scale);
+            alert(e.originalEvent.scale);
         if ( $(this).data("prevented") === true ) {
             $(this).data("prevented", false);
             return;
