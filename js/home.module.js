354bebfc5c84be18deec60fd151e6cba2b7e7e85
bao <thobaocss@gmail.com>
2018-08-31 09:54:34 +0700

    update dhc user

--- js/home.module.js
+++ js/home.module.js
@@ -25,13 +25,13 @@ $(document).ready(function () {
         urlAndroid = 'https://play.google.com/store/apps/details?id=vn.anvui.hotspringpark';
     urlIOs = 'https://itunes.apple.com/us/app/dhc-travel/id1381272202?l=vi&ls=1&mt=8';
     // $('#mapdhc').bind('touchmove', true);
-    document.addEventListener('gesturestart', function (e) {
-        if ( $(this).data("prevented") === true ) {
-            $(this).data("prevented", false);
-            return;
-        }
-        e.preventDefault();
-    });
+    // document.addEventListener('gesturestart', function (e) {
+    //     if ( $(this).data("prevented") === true ) {
+    //         $(this).data("prevented", false);
+    //         return;
+    //     }
+    //     e.preventDefault();
+    // });
     document.addEventListener('gestureend', function (event) {
       alert(window.scaleX);
     });
