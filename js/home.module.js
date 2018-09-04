06cfa5ec7f7adff656a9a02c7a9742cc21f73de5
bao <thobaocss@gmail.com>
2018-09-04 09:22:59 +0700

    update dhc user

--- js/home.module.js
+++ js/home.module.js
@@ -260,7 +260,9 @@ function resetPoint(){
     });
     var html_marker = '';
     var scale=$('#mapdhc').css('transform')!='none'?parseFloat(($('#mapdhc').css('transform')).substring(7,14)):1;
-
+    var p = document.getElementById("mapdhc");
+    var style = p.currentStyle || window.getComputedStyle(p);
+    alert("Current marginTop: " + style.marginTop);
     $.each(pointData, function (k, v) {
         var pointImage = JSON.parse(v.point_images);
         if (pointImage[0] != undefined)
