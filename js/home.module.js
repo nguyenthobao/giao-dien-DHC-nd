c14c622c76d1b5643706ab7ac238b2f4743932a7
bao <thobaocss@gmail.com>
2018-09-10 14:06:15 +0700

    update dhc user

--- js/home.module.js
+++ js/home.module.js
@@ -180,18 +180,14 @@ $(document).ready(function () {
     var hammer = new Hammer(img);
     hammer.get('pinch').set({enable: true});
     hammer.on("pinch", function (e) {
-        // sessionStorage.setItem('scale',e.scale);
-        // $(img).css('transform','scale(' + e.scale + ')');
-        $('.addCanvas').css('transform', 'scale(' + e.scale + ')');
-        $('#scalehammer').val(e.scale);
-        resetPoint();
+        // $('.addCanvas').css('transform', 'scale(' + e.scale + ')');
+        // $('#scalehammer').val(e.scale);
+        // resetPoint();
     });
     hammer.on("pinchend", function (e) {
-        //sessionStorage.setItem('scale',e.scale);
-        // $(img).css('transform','scale(' + e.scale + ')');
-        $('.addCanvas').css('transform', 'scale(' + e.scale + ')');
-        $('#scalehammer').val(e.scale);
-        resetPoint();
+        // $('.addCanvas').css('transform', 'scale(' + e.scale + ')');
+        // $('#scalehammer').val(e.scale);
+        // resetPoint();
     });
     $('body').on('click', '.fixed-top', function () {
         $('html,.containermap,main,.content').removeClass('height-screen');
