8460f0af90bf4a5c59a32b1096e58e8418884b88
bao <thobaocss@gmail.com>
2018-09-12 14:15:46 +0700

    update dhc user

--- js/map.module.js
+++ js/map.module.js
@@ -85,7 +85,7 @@ $(document).ready(function () {
         if (e.touches.length== 1) {
             touchstartX = e.originalEvent.touches[0].pageX;
             touchstartY = e.originalEvent.touches[0].pageY;
-            $('#area').attr('coords', (touchstartX-20) + ',' + (touchstartY - 180) + ',' +150);
+            $('#area').attr('coords',(touchstartX-70)+','+(touchstartY-280)+','+(touchstartX+30)+','+(touchstartY-80));
         }
         // e.reset();
            // $('#choose').val($('#area').attr('coords'));
